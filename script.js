e96acd6dbb55852a3f0a94aa3a9e7a9da21dd6c3
lilifuna <mujinn@interia.pl>
2017-11-08 12:49:13 +0100

    DONE, ale brakuje case i fora xD

--- script.js
+++ script.js
@@ -1,3 +1,31 @@
+let mDown = -1;
+let count = Math.floor(Math.random() * (100 - 0));
+
+function mousedown(event){
+	if(mDown == -1){
+		mDown = setInterval(whilemousedown, 100);
+	}
+}
+
+function mouseup(event){
+	while(mDown != -1){
+		clearInterval(mDown);
+		mDown = -1;
+	}
+}
+
+function whilemousedown(){
+	document.getElementById("counter").innerHTML = count--;
+}
+
+function dblclick(event){
+	document.getElementById("bmiTitle").style.color = "magenta";
+}
+
+document.getElementById("listener").addEventListener("mousedown", mousedown);
+document.getElementById("listener").addEventListener("mouseup", mouseup);
+window.addEventListener("dblclick", dblclick)
+
 function welcome(){
 	window.alert("Witaj w miejscu dla fanów czterokołowych skuterów!")
 	let bestCar = window.prompt("Najlepszy samochód świata to...")
@@ -20,3 +48,21 @@ function hide(element){
 function show(element){
 	element.style.display = 'block'
 }
+
+
+function getBMI(){
+	let form = document.getElementById("bmiForm");
+	let height = parseFloat(form.elements[0].value);
+	let weight = parseInt(form.elements[1].value);
+	let bmi = 0;
+	if(typeof(height) == "number"){
+		if(height <= 0){
+			bmi = "Błędne dane"
+		}
+		else{
+			bmi = Math.floor(weight/(height^2));
+		}
+	}
+	document.getElementById("BMI").innerHTML = bmi;
+}
+

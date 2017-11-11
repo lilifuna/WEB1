58e228aadc290371e3d56c65f7833e20a0bc304a
szymly <szymly@gmail.com>
2017-11-11 16:41:15 +0100

    DOM start

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
+"use strict";
 let mDown = -1;
 let count = Math.floor(Math.random() * (100 - 0));
 
 function mousedown(event){
-	if(mDown == -1){
+	if(mDown === -1){
 		mDown = setInterval(whilemousedown, 100);
 	}
 }
 
 function mouseup(event){
-	while(mDown != -1){
+	while(mDown !== -1){
 		clearInterval(mDown);
 		mDown = -1;
 	}
@@ -32,39 +33,46 @@ function dblclick(event){
 		
 		case 3:
 		document.getElementById("bmiTitle").style.color = "green";
+		break;
 	}
 }
 
 function click(){
-	alert("LICZYMY");
+	window.alert("LICZYMY");
 }
 
 document.getElementById("listener").addEventListener("mousedown", mousedown);
 document.getElementById("listener").addEventListener("mouseup", mouseup);
-window.addEventListener("dblclick", dblclick)
-document.getElementById("button").addEventListener("click", click)
+window.addEventListener("dblclick", dblclick);
+document.getElementById("button").addEventListener("click", click);
+window.addEventListener("beforeunload", zamknij);
+
+
+function zamknij(){
+	let sure = window.confirm("For sure?");
+}
 
 function welcome(){
-	window.alert("Witaj w miejscu dla fanów czterokołowych skuterów!")
-	let bestCar = window.prompt("Najlepszy samochód świata to...")
-	let newsletter = document.getElementById("newsletter")
+	window.alert("Witaj w miejscu dla fanów czterokołowych skuterów!");
+	let bestCar = window.prompt("Najlepszy samochód świata to...");
+	let newsletter = document.getElementById("newsletter");
 
-	if(bestCar == "Matiz" || bestCar == "matiz" || bestCar == "MATIZ"){
-		show(newsletter)
-		alert("Ma się rozumieć!")
+	if(bestCar === "Matiz" || bestCar === "matiz" || bestCar === "MATIZ"){
+		show(newsletter);
+		window.alert("Ma się rozumieć!");
 	}
 	else{
-		hide(newsletter)
+		hide(newsletter);
 	}
 }
 
 
 function hide(element){
-	element.style.display = 'none'
+	element.style.display = "none";
 }
 
 function show(element){
-	element.style.display = 'block'
+	element.style.display = "block";
 }
 
 
@@ -73,9 +81,9 @@ function getBMI(){
 	let height = parseFloat(form.elements[0].value);
 	let weight = parseInt(form.elements[1].value);
 	let bmi = 0;
-	if(typeof(height) == "number"){
+	if(typeof(height) === "number"){
 		if(height <= 0){
-			bmi = "Błędne dane"
+			bmi = "Błędne dane";
 		}
 		else{
 			bmi = Math.floor(weight/(height^2));

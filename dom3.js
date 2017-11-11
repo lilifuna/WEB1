067f0259320241cbae21a2485dd0ca3b57307f61
szymly <szymly@gmail.com>
2017-11-11 19:45:11 +0100

    DOM

--- dom3.js
+++ dom3.js
@@ -1,1 +1,39 @@
 "use strict";
+
+function style(){
+	let fontType = document.getElementById("fontType").selectedIndex;
+	let bgColor = document.getElementById("bgColor").selectedIndex;
+	let fontColor = document.getElementById("fontColor").selectedIndex;
+
+	alert(fontType)
+
+	switch(fontType){
+		case 0:
+		document.body.style.fontFamily: "bebasregular";
+		break;
+
+		case 1:
+		document.body.style.fontFamily: "bebasregular";
+		break;
+	}
+
+	switch(bgColor){
+		case 0:
+		document.body.style.backgroundColor = "white";
+		break;
+
+		case 1:
+		document.body.style.backgroundColor = "gray";
+		break;
+	}
+
+	switch(fontColor){
+		case 0:
+		document.body.style.color = "black";
+		break;
+
+		case 1:
+		document.body.style.color = "white";
+		break;
+	}
+}
